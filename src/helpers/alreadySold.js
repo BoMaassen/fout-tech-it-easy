b18281b6f091300586a7b97a9727d9fa0a8b57ed
BoMaassen <bomaassen@live.nl>
2024-11-11 12:24:37 +0100

    opdracht 1a

--- src/helpers/alreadySold.js
+++ src/helpers/alreadySold.js
@@ -1,9 +1,11 @@
 import {inventory} from "../constants/inventory.js";
 
 function alreadySold(){
-return (
-    console.log("test")
-)
+    let soldTv = 0;
+    for (let i = 0; i < inventory.length ; i ++){
+        soldTv = soldTv + inventory[i].sold
+    }
+   return console.log(soldTv);
 }
 
 export default alreadySold;
